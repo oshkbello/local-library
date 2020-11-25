2bbdb4b213ac8b5364c255fecc4e003cf9acf1c2
Oshioke Bello <bellozeekar@gmail.com>
2020-11-25 15:45:46 -0800

    Installed mongoose package to access mongoDB and Created models for local-library

---
+++ models/genre.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Mongoose.Schema;
+
+let GenreSchema = new Schema({
+  name: { type: String, required: true, maxlength:100, minlength:3 }
+});
+
+//virtual for genre url
+
+GenreSchema.virtual('url').get(function () {
+  return '/catalog/bookinstance/' + this._id;
+});
+
+//export module
+module.exports = mongoose.model('GenreSchema', GenreSchema);
