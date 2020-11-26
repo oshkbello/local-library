a1112c9617943f7482ef7dd3d63f13ea55114435
Oshioke Bello <bellozeekar@gmail.com>
2020-11-25 19:33:07 -0800

    created model for genre, created an independent script to test created models

--- models/genre.js
+++ models/genre.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Mongoose.Schema;
+const Schema = mongoose.Schema;
 
 let GenreSchema = new Schema({
   name: { type: String, required: true, maxlength:100, minlength:3 }
