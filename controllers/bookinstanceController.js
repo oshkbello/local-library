aa3a672b07cf94720cddc5d344f53498e6c6bf04
Oshioke Bello <bellozeekar@gmail.com>
2020-11-28 12:41:14 -0800

    Added functionality to the empty controllers. Also created an index function in the Book controller to act as the entry point into our local library

---
+++ controllers/bookinstanceController.js
@@ -0,0 +1,39 @@
+const BookInstance = require('../models/bookinstance');
+
+exports.bookinstance_list = function(req, res) {
+  res.send('NOT IMPLEMENTED" BookIntance list');
+}
+
+exports.bookinstance_details = function(req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance details: ' + req.params.id);
+}
+
+// Display BookInstance create form on GET.
+exports.bookinstance_create_get = function(req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance create GET');
+};
+
+// Handle BookInstance create on POST.
+exports.bookinstance_create_post = function(req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance create POST');
+};
+
+// Display BookInstance delete form on GET.
+exports.bookinstance_delete_get = function(req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance delete GET');
+};
+
+// Handle BookInstance delete on POST.
+exports.bookinstance_delete_post = function(req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance delete POST');
+};
+
+// Display BookInstance update form on GET.
+exports.bookinstance_update_get = function(req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance update GET');
+};
+
+// Handle bookinstance update on POST.
+exports.bookinstance_update_post = function(req, res) {
+  res.send('NOT IMPLEMENTED: BookInstance update POST');
+};
